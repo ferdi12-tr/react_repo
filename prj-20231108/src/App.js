0cf15d489b426d2ef710122172595fc81ef033e6
fkoca <ferdikoca12@gmail.com>
2023-11-08 11:55:53 +0300

    print lists with states

--- prj-20231108/src/App.js
+++ prj-20231108/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import CountFunction from './Counter';
+import TodoList from './TodoList';
 
 function App() {
   return (
     <div className="container">
       <div className='row'>
         <div className='col-4'><CountFunction/></div>
-        <div className='col-4'></div>
+        <div className='col-4'><TodoList/></div>
         <div className='col-4'></div>
       </div>
     </div>
--- prj-20231108/src/TodoList.js
+++ prj-20231108/src/TodoList.js
@@ -1,5 +1,31 @@
 import React, {useState} from "react";
 
 export default function TodoList() {
-    
+    const [tdList, setTdList] = useState([]);
+    const [todo, setTodo] = useState("");
+
+    const addTodoList = () => {
+        if (todo) {
+            setTdList([...tdList, todo])
+            setTodo("")
+        }
+    }
+
+    const getTodo = (value) => {
+        setTodo(value)
+    }
+
+
+    return (
+        <div>
+            <h3>Görev Listesi</h3>
+            <div className="input-group mb-3">
+                <input type="text" className="form-control" onChange={(event) => getTodo(event.target.value)}></input>
+                <button className="btn btn-outline-secondary" type="button" onClick={addTodoList}>Ekle</button>
+            </div>
+            <ul>
+                {tdList.map(element => <li>{element}</li>)}
+            </ul>
+        </div>
+    )
 }

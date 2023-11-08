366612ea1b7ae697c6b5b39fc62d47b138247ae1
fkoca <ferdikoca12@gmail.com>
2023-11-08 10:05:31 +0300

    increment decrement operation with states

---
+++ prj-20231108/src/Counter.js
@@ -0,0 +1,30 @@
+import React, {useState} from "react";
+
+export default function CountFunction() {
+    const [count, setCount] = useState(0);
+
+
+    const increment = () => {
+        setCount(count + 1);
+    }
+
+    const decrement = () => {
+        setCount(count - 1);
+    }
+
+    return (
+        <div>
+            <p>Count: {count}</p>
+            <button 
+                className="btn btn-primary" 
+                type="button" 
+                onClick={increment}
+            >Increment</button>
+            <button 
+                className="btn btn-danger" 
+                type="button" 
+                onClick={decrement}
+            >Decrement</button>
+        </div>
+    )
+}

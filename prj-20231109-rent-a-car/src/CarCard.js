22a14b4502064783aa2aa6fc89cdb92dd8a1b751
fkoca <ferdikoca12@gmail.com>
2023-11-10 22:36:52 +0300

    add car description and date input area to modal

--- prj-20231109-rent-a-car/src/CarCard.js
+++ prj-20231109-rent-a-car/src/CarCard.js
@@ -1,15 +1,24 @@
 import React, {useState} from "react";
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Input } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, FormGroup, Label, Input } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function CarCard({car}) {
 
     const [modal, setModal] = useState(false);
+    const [currentCar, setCurrentCar] = useState(null)
 
-    const toggle = () => setModal(!modal);
+    const toggleModal = (car=null) => {
+        setModal(!modal)
+        if(car) {
+            setCurrentCar(car)
+        }
+        else {
+            setCurrentCar(null)
+        }
+    };
 
     return (
-        <><button onClick={toggle} style={{border:"none", background:"none", padding:0} }>
+        <><button onClick={() => toggleModal(car)} style={{border:"none", background:"none", padding:0} }>
 
             <div className="card text-bg-dark mb-3">
                 <img src={car.carUrl} className="card-img" height={"150px"} style={{opacity: 0.7}} alt="..."/>
@@ -21,29 +30,28 @@ export default function CarCard({car}) {
 
         </button>
         <div>
-            {/* <Button color="danger" onClick={toggle}>
-            Click Me
-            </Button> */}
-            <Modal isOpen={modal} toggle={toggle} backdrop={true} fade={true}>
-            <Input type='text'/>
-            <ModalHeader toggle={toggle}>Modal title</ModalHeader>
-            <ModalBody>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                aliquip ex ea commodo consequat. Duis aute irure dolor in
-                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                culpa qui officia deserunt mollit anim id est laborum.
-            </ModalBody>
-            <ModalFooter>
-                <Button color="primary" onClick={toggle}>
-                Do Something
-                </Button>{' '}
-                <Button color="secondary" onClick={toggle}>
-                Cancel
-                </Button>
-            </ModalFooter>
+            <Modal isOpen={modal} toggle={toggleModal} backdrop={true} fade={true}>
+                <ModalHeader toggle={toggleModal}>{"Car Brand: " + currentCar.carBrand + " Car Model: " + currentCar.carModel}</ModalHeader>
+                <ModalBody>
+                    To preceed booking this car, please enter the date to calculate pay amount.
+                    Car Per Hour Price: {car.price}
+                    <FormGroup>
+                        <Label for="exampleDate">Date From: </Label>
+                        <Input id="exampleDate" name="date" placeholder="date placeholder" type="date"/>
+
+                        <Label for="exampleDate"> Date To: </Label>
+                        <Input id="exampleDate" name="date"  placeholder="date placeholder" type="date"/>
+                    </FormGroup>
+                </ModalBody>
+
+                <ModalFooter>
+                    <Button color="success" onClick={toggleModal}>
+                        Book
+                    </Button>
+                    <Button color="danger" onClick={toggleModal}>
+                        Cancel
+                    </Button>
+                </ModalFooter>
             </Modal>
         </div>
         </>

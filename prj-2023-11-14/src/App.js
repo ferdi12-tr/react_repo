d526e501d6e6e6bc9558678a3e96b05c5eb046cf
fkoca <ferdikoca12@gmail.com>
2023-11-14 14:28:04 +0300

    Add component created

--- prj-2023-11-14/src/App.js
+++ prj-2023-11-14/src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {   
+  Container,
+  Row,
+  Col,
+} from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './Header'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+
+  state = {
+    currentCategory: "",
+    products: [],
+    cart: [],
+  };
+
+  chanceCategory = (category) => {
+    this.setState({ currentCategory: category.categoryName });
+    this.getProducts(category.id);
+  };
+
+  getProducts = (categoryId) => {
+    let url = "http://localhost:3000/products";
+    if (categoryId) {
+      url += "?categoryId=" + categoryId;
+    }
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => this.setState({ products: data }));
+  };
+
+  componentDidMount() {
+    this.getProducts();
+  }
+
+  addToCart = (product) => {
+    let newCart = this.state.cart;
+    var addedItem = newCart.find((c) => c.product.id === product.id);
+    if (addedItem) {
+      addedItem.quantity += 1;
+    } else {
+      newCart.push({ product: product, quantity: 1 });
+    }
+    this.setState({ cart:  newCart });
+  };
+
+  removeToCart = (product) => {
+    let newCart = this.state.cart.filter((c) => c.product.id !== product.id);
+    this.setState({ cart: newCart });
+  };
+
+  render() {
+    return (
+      <Container>
+        <Row>
+        <Header cart={this.state.cart} removeToCart={this.removeToCart} />
+        </Row>
+        <Row>
+          <Col xs="3">
+            category
+          </Col>
+          <Col xs="9">
+            category
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
-
-export default App;

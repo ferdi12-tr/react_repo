7247f2f469fa37ba5579dbcbafa9c2418d56bbe1
fkoca <ferdikoca12@gmail.com>
2023-11-15 11:08:03 +0300

    change to class component and load images from json file

--- prj-20231109-rent-a-car/src/MainPage.js
+++ prj-20231109-rent-a-car/src/MainPage.js
@@ -1,63 +1,78 @@
-import React, {useState} from "react";
+import React, { Component } from "react";
 import CarCard from "./CarCard";
 import CarNavBar from "./CarNavBar";
 
 
-export default function MainPage() {
+export default class MainPage extends Component {
 
-    const [id, setId] = useState(0);
-    const [carBrand, setCarBrand] = useState("");
-    const [carModel, setCarModel] = useState("");
-    const [carPrice, setCarPrice] = useState("")
-    const [carUrl, setCarUrl] = useState("");
-
-    const [carList, setCarList] = useState([]);
-
-    const addCar = () => {
-        if (carBrand && carModel && Number(carPrice) > 0) {
-            setId(id + 1)
-            setCarList([...carList, {id, carBrand, carModel, carPrice, carUrl}])
+    constructor(props) {
+        super(props);
+        this.state = {
+            id: 0,
+            carBrand: "",
+            carModel: "",
+            carPrice: "",
+            carUrl: "",
+            carList: []
         }
-        setCarBrand("");
-        setCarModel("");
-        setCarPrice("");
-        setCarUrl("");
-
-        console.log(carList)
     }
 
-    const GetCarUrl = (file) => {
+    componentDidMount() {
+        let db_url = "http://localhost:3000/cars"
+
+        fetch(db_url)
+            .then(db => db.json())
+                .then(data => {
+                    this.setState({carList: data})
+                })
+    }
+
+    addCar = () => {
+        if (this.state.carBrand && this.state.carModel && Number(this.state.carPrice) > 0) {
+            this.setState({ id: this.state.id + 1 })
+            // setCarList([...this.state.carList, { id, carBrand, carModel, carPrice, carUrl }])
+            this.state.carList.push({ id: this.state.id, carBrand: this.state.carBrand, carModel: this.state.carModel, carPrice: this.state.carPrice, carUrl: this.state.carUrl })
+        }
+        this.setState({ carBrand: "" });
+        this.setState({ carModel: "" });
+        this.setState({ carPrice: "" });
+        this.setState({ carUrl: "" });
+    }
+
+    GetCarUrl = (file) => {
         let url = URL.createObjectURL(file);
-        setCarUrl(url);
+        this.setState({ carUrl: url });
     }
 
-    return (
-        <>
-            <CarNavBar/>
-            <div className="container">
-                <div className="row mt-3">
-                    <div className="col-3">
-                        <input type="text" className="form-control mt-3" value={carBrand} onChange={(e) => setCarBrand(e.target.value)} placeholder="Brand" />
-                        <input type="text" className="form-control mt-3" value={carModel} onChange={(e) => setCarModel(e.target.value)} placeholder="Model" />
-                        <input type="number" className="form-control mt-3" value={carPrice} onChange={(e) => setCarPrice(e.target.value)} placeholder="Price (Per Hour)" />
+    render() {
+        return (
+            <>
+                <CarNavBar />
+                <div className="container">
+                    <div className="row mt-3">
+                        <div className="col-3">
+                            <input type="text" className="form-control mt-3" value={this.state.carBrand} onChange={(e) => this.setState({ carBrand: e.target.value })} placeholder="Brand" />
+                            <input type="text" className="form-control mt-3" value={this.state.carModel} onChange={(e) => this.setState({ carModel: e.target.value })} placeholder="Model" />
+                            <input type="number" className="form-control mt-3" value={this.state.carPrice} onChange={(e) => this.setState({ carPrice: e.target.value })} placeholder="Price (Per Hour)" />
 
-                        <label htmlFor="imgUploader" className="btn btn-danger btn-sm mt-3">Upload Image</label>
-                        <input style={{ display: "none" }} id="imgUploader" type="file" name="Image" onChange={(event) => { GetCarUrl(event.target.files[0]); } } /> <br />
+                            <label htmlFor="imgUploader" className="btn btn-danger btn-sm mt-3">Upload Image</label>
+                            <input style={{ display: "none" }} id="imgUploader" type="file" name="Image" onChange={(event) => { this.GetCarUrl(event.target.files[0]); }} /> <br />
 
-                        <button className="btn btn-success mt-3" onClick={addCar}>Add Car</button>
-                    </div>
-                    <div className="col-9">
-                        <div className="container">
-                            <div className="row">
-                                {carList.map((element, index) => <div key={index} className="col-6">
-                                    <CarCard car={element} />
+                            <button className="btn btn-success mt-3" onClick={this.addCar}>Add Car</button>
+                        </div>
+                        <div className="col-9">
+                            <div className="container">
+                                <div className="row">
+                                    {this.state.carList.map((element, index) => <div key={index} className="col-6">
+                                        <CarCard car={element} />
+                                    </div>
+                                    )}
                                 </div>
-                                )}
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </>
-    )
+            </>
+        )
+    }
 }

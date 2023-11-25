90b89459c735bf84e783947d9ce8e3aacdd7c12c
fkoca <ferdikoca12@gmail.com>
2023-11-25 08:44:28 +0300

    add bag dropdown to show booked car info

--- prj-20231109-rent-a-car/src/CarNavBar.js
+++ prj-20231109-rent-a-car/src/CarNavBar.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
-import { Navbar, NavbarBrand, NavbarToggler, Collapse, Nav, NavItem, NavLink } from 'reactstrap';
+import { Button, Navbar, NavbarBrand, NavbarToggler, Collapse, Nav, NavItem, NavLink, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
 import logo from './fkoca_logo.svg';
-import {store, loginUserStore} from "./redux/store";
+import { store, loginUserStore } from "./redux/store";
 
 
 export default class CarNavBar extends Component {
@@ -20,11 +22,11 @@ export default class CarNavBar extends Component {
     }
 
     // when user logged in, show users booked cars info up on dropdown
-    whenLogin = () => { 
+    whenLogin = () => {
         let addedCarsIdList = loginUserStore.getState()[0].currentUser.addedCarsId;
-        let bookedCars = this.props.getCarList().filter(car =>  addedCarsIdList.includes(car.id));
-        this.setState({bookedCarList:bookedCars})
-        
+        let bookedCars = this.props.getCarList().filter(car => addedCarsIdList.includes(car.id));
+        this.setState({ bookedCarList: bookedCars })
+
     }
 
     calculateTotal = () => {
@@ -35,7 +37,7 @@ export default class CarNavBar extends Component {
         });
         this.setState({ totalPayAmount: total });
     }
-    
+
 
     /* FlexGrow is a temporary solution for aligning item to the right side*/
     render() {
@@ -64,9 +66,20 @@ export default class CarNavBar extends Component {
                             <NavItem>
                                 <NavLink href="#">Sign Up</NavLink>
                             </NavItem>
-                            <NavItem>
-                                <NavLink href="#">Bag: {this.state.totalPayAmount}$</NavLink>
-                            </NavItem>
+                            
+                            <UncontrolledDropdown nav inNavbar>
+                                <DropdownToggle nav caret>
+                                    {this.state.totalPayAmount}$
+                                </DropdownToggle>
+                                <DropdownMenu end>
+                                    {this.state.bookedCarList.map((element, index) =>
+                                        <DropdownItem key={index}>{element.carModel} {element.carBrand}<FontAwesomeIcon onClick={() => console.log(element)} color='red' className='ms-3' icon={faTrash} /></DropdownItem>
+                                    )}
+                                    <DropdownItem divider />
+                                    <DropdownItem tag="a"><Button size='sm' color='danger' disabled={!(this.state.bookedCarList.length > 0)}>Reset</Button></DropdownItem>
+                                    <DropdownItem tag="a"><Button size='sm' disabled={!(this.state.bookedCarList.length > 0)} color='success'>Buy All</Button></DropdownItem>
+                                </DropdownMenu>
+                            </UncontrolledDropdown>
                         </Nav>
                     </Collapse>
                 </Navbar>

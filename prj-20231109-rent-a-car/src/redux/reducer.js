720b75f3d6b417702d01ebc14bbe87bcd2ca8dec
fkoca <ferdikoca12@gmail.com>
2023-11-27 19:27:16 +0300

    hold the state without list

--- prj-20231109-rent-a-car/src/redux/reducer.js
+++ prj-20231109-rent-a-car/src/redux/reducer.js
@@ -1,18 +1,18 @@
 import * as actions from './actionTypes'
 
-export default function reducer(state = [], action) {
+export default function reducer(state = {}, action) {
     switch (action.type) {
         case actions.CAR_ADDED:
-            return [
-                ...state,
-                {
-                    car:action.payload.car,
-                    totalHour:action.payload.totalHour,
+            return  {
+                        ...state,
+                        car:action.payload.car,
+                        totalHour:action.payload.totalHour,
                 }
-            ]
+            
         case actions.CAR_REMOVED:
             return state.filter(item => item.car.id !== action.payload.id)
         default:
             return state;
     }
+    
 }
